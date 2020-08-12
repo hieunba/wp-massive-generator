ed781e53622d73c28d9d7c796141424fed51db99
Hieu <hi3unb@gmail.com>
2020-08-13 01:39:37 +0700

    Update log messages

--- generator/start.sh
+++ generator/start.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 set -eou pipefail
 
-docker_tag="mli/generator"
+docker_tag=mli/generator
 
-log() {
-  echo "GENERATOR :: INFO :: $1"
+banner() {
+  echo -n " * "
 }
 
-log_error() {
-  echo "GENERATOR :: ERROR :: $1"
+log_info_msg() {
+  banner
+  echo "$@"
+}
+
+log_success_msg() {
+  local GREEN=`tput setaf 3`
+  local NORMAL=`tput op`
+  echo " $GREEN*$NORMAL $@"
+  exit 0
+}
+
+log_error_msg() {
+  local RED=`tput setaf 1`
+  local NORMAL=`tput op`
+  echo " $RED*$NORMAL $@"
   exit 1
 }
 
 check_docker() {
-  command -v docker >/dev/null || log_error 'Docker was not found'
+  command -v docker >/dev/null || log_error_msg 'Docker was not found'
 }
 
 check_image() {
   set -ex; docker inspect $docker_tag > /dev/null || {
-    log 'Building Docker image..'
+    log_info_msg 'Building Docker image..'
     build_image
   }
 }
