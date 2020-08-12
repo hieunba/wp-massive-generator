38602331b7cb2a37d1577867fab1da1872766da1
Hieu <hi3unb@gmail.com>
2020-08-13 02:19:30 +0700

    Load defaults values

--- generator/start.sh
+++ generator/start.sh
@@ -27,6 +27,31 @@ log_error_msg() {
   exit 1
 }
 
+load_defaults() {
+  AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-us-west-2}"
+  AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-}"
+  AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-}"
+  EC2_INSTANCE_TYPE="${EC2_INSTANCE_TYPE:-t2.small}"
+  EC2_KEYPAIR="${EC2_KEYPAIR:-mli}"
+  WP_HAS_CDN="${WP_HAS_CDN:-false}"
+  WP_HAS_LB="${WP_HAS_LB:-false}"
+  WP_SITEURL="${WP_SITEURL:-}"
+  WP_HOME="${WP_HOME:-}"
+
+  if [ "x${ENV,,}" == "xdev" ] ; then
+    log_info_msg "Defaults values"
+    typeset -p AWS_DEFAULT_REGION \
+               AWS_ACCESS_KEY_ID \
+               AWS_SECRET_ACCESS_KEY \
+               EC2_INSTANCE_TYPE \
+               EC2_KEYPAIR \
+               WP_HAS_CDN \
+               WP_HAS_LB \
+               WP_SITEURL \
+               WP_HOME
+  fi
+}
+
 check_docker() {
   command -v docker >/dev/null || log_error_msg 'Docker was not found'
 }
@@ -48,5 +73,6 @@ build_image() {
 }
 
 # __MAIN__
+load_defaults
 check_docker
 check_image
