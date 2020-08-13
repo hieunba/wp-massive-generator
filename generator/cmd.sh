25ce8902666ab7a82237f757c5af7c8d96d380c8
Hieu <hi3unb@gmail.com>
2020-08-13 07:33:15 +0700

    Add log functions to default command of Docker container

--- generator/cmd.sh
+++ generator/cmd.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+banner() {
+  echo -n " * "
+}
+
+log_info_msg() {
+  banner
+  echo "$@"
+}
+
+log_success_msg() {
+  local GREEN=`tput setaf 3`
+  local NORMAL=`tput op`
+  echo " $GREEN*$NORMAL $@"
+  exit 0
+}
+
+log_error_msg() {
+  local RED=`tput setaf 1`
+  local NORMAL=`tput op`
+  echo " $RED*$NORMAL $@"
+  exit 1
+}
