ccd97949bc9b987147ddbedfda35f60072e9b907
Hieu <hi3unb@gmail.com>
2020-08-13 15:32:02 +0700

    Add application load balancer and related components

--- generator/terraform/generator/main.tf
+++ generator/terraform/generator/main.tf
@@ -96,3 +96,49 @@ resource "aws_security_group" "allow_web_vpc" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_lb" "wp-http" {
+  name               = "wp-http"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_web_alb.id]
+  subnets            = var.public_subnets
+
+  tags = {
+    Name = "wp-stack"
+  }
+}
+
+resource "aws_lb_target_group" "wp-http" {
+  name                               = "wp-http"
+  port                               = 80
+  protocol                           = "HTTP"
+  vpc_id                             = module.vpc.vpc_id
+  lambda_multi_value_headers_enabled = false
+  proxy_protocol_v2                  = false
+
+  health_check {
+    path                = "/"
+    matcher             = "200,301,302"
+    healthy_threshold   = 5
+    unhealthy_threshold = 5
+    timeout             = 6
+  }
+
+  stickiness {
+    type            = "lb_cookie"
+    cookie_duration = 3600
+    enabled         = true
+  }
+}
+
+resource "aws_lb_listener" "wp-http" {
+  load_balancer_arn = aws_lb.wp-http.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wp-http.arn
+  }
+}
