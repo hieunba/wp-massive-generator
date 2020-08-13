c155f2af9ede572a9057791e487f29c1ac80b4d3
Hieu <hi3unb@gmail.com>
2020-08-13 17:02:46 +0700

    Allow SSH to EC2 instances

--- generator/terraform/generator/main.tf
+++ generator/terraform/generator/main.tf
@@ -170,7 +170,7 @@ resource "aws_launch_configuration" "wp" {
   instance_type = var.instance_type
   key_name      = var.key_name == "" ? var.default_key_name : var.key_name
 
-  security_groups = [aws_security_group.allow_web_vpc.id]
+  security_groups = [aws_security_group.allow_web_vpc.id, aws_security_group.allow_ssh.id]
 
   associate_public_ip_address = true
 
