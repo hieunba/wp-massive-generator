d3e775abc33119b00adf7db9024fc3534426499e
Hieu <hi3unb@gmail.com>
2020-08-15 09:45:46 +0700

    Populate additional Terraform variables

--- generator/entrypoint.sh
+++ generator/entrypoint.sh
@@ -4,5 +4,8 @@ set -euo pipefail
 if [ "$AWS_DEFAULT_REGION" ] ; then
   export TF_VAR_region=$AWS_DEFAULT_REGION
 fi
+export TF_VAR_wp_siteurl=$WP_SITEURL
+export TF_VAR_instance_type=$EC2_INSTANCE_TYPE
+export TF_VAR_autoscale_min_size=$EC2_INSTANCE_NO
 
 exec "$@"
