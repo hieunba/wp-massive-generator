b8baf14e7e8770f9070df7e4e7a5393d7a9777fb
Hieu <hi3unb@gmail.com>
2020-08-18 03:30:49 +0700

    Compress objects that hit Cloudfront distribution

--- generator/terraform/generator/main.tf
+++ generator/terraform/generator/main.tf
@@ -327,6 +327,7 @@ resource "aws_cloudfront_distribution" "wp_distribution" {
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
+    compress               = true
   }
 
   restrictions {
