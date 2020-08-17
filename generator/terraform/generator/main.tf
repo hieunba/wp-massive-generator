62bf882db6b2819ee532ada795dc40d4d15594c9
Hieu <hi3unb@gmail.com>
2020-08-18 01:37:46 +0700

    CloudFront distribution

--- generator/terraform/generator/main.tf
+++ generator/terraform/generator/main.tf
@@ -283,3 +283,58 @@ resource "aws_db_parameter_group" "wp" {
     value = 1
   }
 }
+
+resource "aws_cloudfront_distribution" "wp_distribution" {
+  origin {
+    domain_name = aws_lb.wp-http.dns_name
+    origin_id   = "wp-application"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "match-viewer"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "wp-application"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "VN"]
+    }
+  }
+
+  tags = {
+    Name = "WP"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  depends_on = [aws_lb.wp-http]
+}
